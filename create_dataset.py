7f87329e764fd97b646a94ba7c87d5a68a9f52e4
zyon <32070378+zyoNoob@users.noreply.github.com>
2025-02-17 22:25:53 +0530

    added first code to print a sampled image

--- create_dataset.py
+++ create_dataset.py
@@ -1,2 +1,14 @@
 #!.venv/bin/python
-print("hello")
+from torchcodec.decoders import VideoDecoder
+from PIL import Image
+
+device = "cuda"
+
+decoder = VideoDecoder("data/input.mp4", device=device)
+
+print(f"Video Metatada -\n{decoder.metadata}")
+
+sampled_frame = decoder[0]
+
+image = Image.fromarray(sampled_frame.cpu().detach().numpy().transpose(1,2,0))
+image.show()
